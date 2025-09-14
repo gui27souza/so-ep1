aaa0ea5211b862de30c43ae47e0fd9aa173063f6
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-09-14 11:15:01 -0300

    adiciona main e loadPrograms em Escalonador

--- src/com/escalonador/Escalonador.java
+++ src/com/escalonador/Escalonador.java
@@ -1,7 +1,8 @@
 package com.escalonador;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Escalonador {
 
@@ -12,5 +13,60 @@ public class Escalonador {
 
 	int quantum;
 
+	public static void main(String[] args) {
+		Escalonador escalonador = new Escalonador();
+
+		escalonador.loadPrograms();
+	}
+
+	public Escalonador() {
+		this.proccessTables = new ArrayList<>();
+		this.readyQueue = new LinkedList<>();
+		this.blockedQueue = new LinkedList<>();
+	}
+
+	public void loadPrograms() {
+		File programsDirectory = new File("programas");
+		File[] programFiles = programsDirectory.listFiles();
+
+		if (programFiles != null) {
+
+			ArrayList<File> filesList = new ArrayList<>(Arrays.asList(programFiles));
+			filesList.sort(Comparator.comparing(File::getName));
+
+			for (File file : filesList) {
+				if (file.isFile() && file.getName().endsWith(".txt")) {
+					try {
+						Scanner scanner = new Scanner(file);
+
+						if (file.getName().equals("quantum.txt")) {
+							this.quantum = Integer.parseInt(scanner.nextLine());
+							scanner.close();
+						} else {
+
+							String programName = scanner.nextLine();
+							ArrayList<String> instructions = new ArrayList<>();
+							while (scanner.hasNextLine()) {
+								instructions.add(scanner.nextLine());
+							}
+							scanner.close();
+
+							BCP newBCP = new BCP();
+							newBCP.setProgramName(programName);
+							newBCP.setInstructions(instructions.toArray(new String[0]));
+
+							proccessTables.add(newBCP);
+							readyQueue.add(newBCP);
+						}
+
+						System.out.println("Arquivo "+file.getName()+" lido com sucesso!");
+
+					} catch (FileNotFoundException e) {
+						System.err.println("Arquivo não encontrado: " + file.getName());
+					}
+				}
+			}
+		}
+	}
 
 }
