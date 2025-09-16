839f7a3b540fd6f534cd26f9cbab04badd114d22
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-09-16 09:25:04 -0300

    finaliza os atributos do BCP

--- src/com/escalonador/BCP.java
+++ src/com/escalonador/BCP.java
@@ -2,14 +2,21 @@ package com.escalonador;
 
 public class BCP {
 
-	String programName;
-	EstadoProcesso state;
-	int programCounter;
-	int registerX;
-	int registerY;
-	String[] instructions;
+	private String programName;
+	private EstadoProcesso state;
+	private int programCounter;
+	private int registerX;
+	private int registerY;
+	private String[] instructions;
+	private int waitTime;
 
-	public BCP() {}
+	public BCP() {
+		this.state = EstadoProcesso.PRONTO;
+		this.programCounter = 0;
+		this.registerX = 0;
+		this.registerY = 0;
+		waitTime = 0;
+	}
 
 	void setProgramName(String programName) { this.programName = programName; }
 	void setState(EstadoProcesso state) { this.state = state; }
@@ -17,6 +24,7 @@ public class BCP {
 	void setRegisterX(int registerX) { this.registerX = registerX; }
 	void setRegisterY(int registerY) { this.registerY = registerY; }
 	void setInstructions(String[] instructions) { this.instructions = instructions; }
+	void setWaitTime(int waitTime) { this.waitTime = waitTime; }
 
 	String getProgramName() { return programName; }
 	EstadoProcesso getState() { return state; }
@@ -24,4 +32,6 @@ public class BCP {
 	int getRegisterX() { return registerX; }
 	int getRegisterY() { return registerY; }
 	String[] getInstructions() { return instructions; }
+	int getWaitTime() { return waitTime; }
+
 }
