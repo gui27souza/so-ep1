68a84d3ea1422aed2e04407222d2e3ad5fd9dfb2
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-10-04 22:51:35 -0300

    adiciona diretório de saída para logXX.txt

--- src/com/escalonador/core/Logger.java
+++ src/com/escalonador/core/Logger.java
@@ -1,5 +1,6 @@
 package com.escalonador.core;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,19 +8,25 @@ import java.io.PrintWriter;
 public class Logger {
     
     private PrintWriter printWriter;
+		private String outputDirectory = "output";
 
-    public Logger(int quantum) {
-        String quantumFormatado = String.format("%02d", quantum);
-        String fileName = "log" + quantumFormatado + ".txt";
-        
-        try {
-            FileWriter fileWriter = new FileWriter(fileName);
-            this.printWriter = new PrintWriter(fileWriter);
-        } catch (IOException e) {
-            System.err.println("Erro ao criar o arquivo de log: " + e.getMessage());
-            this.printWriter = null;
-        }
-    }
+	public Logger(int quantum) {
+		String quantumFormatado = String.format("%02d", quantum);
+		String fileName = "log" + quantumFormatado + ".txt";
+
+		File directory = new File(outputDirectory);
+		if (!directory.exists()) {
+			directory.mkdirs();
+		}
+
+		try {
+			FileWriter fileWriter = new FileWriter(outputDirectory + "/" + fileName);
+			this.printWriter = new PrintWriter(fileWriter);
+		} catch (IOException e) {
+			System.err.println("Erro ao criar o arquivo de log: " + e.getMessage());
+			this.printWriter = null;
+		}
+	}
 
     public void log(String message) {
         if (printWriter != null) {
